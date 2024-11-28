6bea16f55e8e00081a04cde920ce9d395669b026
Leander26 <jensleander26@icloud.com>
2024-11-28 12:59:49 +0100

    Final changes

---
+++ Application/src/App.java
@@ -0,0 +1,13 @@
+import Interfaces.StorageInterface;
+import Storage.Storage;
+import Storage.initStorage;
+
+public class App {
+    public static void main(String[] args) {
+        StorageInterface storage = new Storage();
+        Controllers.Production.setStorage(storage);
+        Controllers.Warehousing.setStorage(storage);
+        Controllers.Common.setStorage(storage);
+        initStorage.loadProduction();
+    }
+}
