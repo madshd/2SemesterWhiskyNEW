2803df8c44f45be0b11c42d49b5d2759734f5f40
Mads D <42411664+madshd@users.noreply.github.com>
2024-11-29 11:30:08 +0100

    Update of the essentials to make it possible to create various warehousing objects and storing them in a test environment.

--- Application/src/Storage/Storage.java
+++ Application/src/Storage/Storage.java
@@ -3,6 +3,9 @@ package Storage;
 import Interfaces.StorageInterface;
 import Production.Distillate;
 import Warehousing.Cask;
+import Warehousing.Ingredient;
+import Warehousing.StorageRack;
+import Warehousing.Warehouse;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -11,6 +14,9 @@ import java.util.List;
 public class Storage implements StorageInterface, Serializable {
     private List<Distillate> distillates = new ArrayList<>();
     private List<Cask> casks = new ArrayList<>();
+    private List<Warehouse> warehouses = new ArrayList<>();
+    private List<StorageRack> storageRacks = new ArrayList<>();
+    private List<Ingredient> ingredients = new ArrayList<>();
 
     @Override
     public List<Distillate> getDistillates() {
@@ -40,5 +46,50 @@ public class Storage implements StorageInterface, Serializable {
     @Override
     public void deleteCask(Cask cask) {
         casks.remove(cask);
+    }
+
+    @Override
+    public List<Warehouse> getWarehouses() {
+        return new ArrayList<>(warehouses);
+    }
+
+    @Override
+    public void storeWarehouse(Warehouse warehouse) {
+        warehouses.add(warehouse);
+    }
+
+    @Override
+    public void deleteWarehouse(Warehouse warehouse) {
+        warehouses.remove(warehouse);
+    }
+
+    @Override
+    public List<StorageRack> getStorageRacks() {
+        return new ArrayList<>(storageRacks);
+    }
+
+    @Override
+    public void storeStorageRack(StorageRack storageRack) {
+        storageRacks.add(storageRack);
+    }
+
+    @Override
+    public void deleteStorageRack(StorageRack storageRack) {
+        storageRacks.remove(storageRack);
+    }
+
+    @Override
+    public List<Ingredient> getIngredients() {
+        return new ArrayList<>(ingredients);
+    }
+
+    @Override
+    public void storeIngredient(Ingredient ingredient) {
+        ingredients.add(ingredient);
+    }
+
+    @Override
+    public void deleteIngredient(Ingredient ingredient) {
+        ingredients.remove(ingredient);
     }
 }
