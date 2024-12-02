d6e4a574b2a342bbf3fcc663fc550ad5176f36e7
Mads D <42411664+madshd@users.noreply.github.com>
2024-12-02 08:49:10 +0100

    Update Item.java

--- Application/src/Interfaces/Item.java
+++ Application/src/Interfaces/Item.java
@@ -1,9 +1,8 @@
 package Interfaces;
 
-public interface Item extends Comparable<Item> {
 import Warehousing.StorageRack;
 
-public interface Item {
+public interface Item extends Comparable<Item>{
     public String getListInfo();
     public double getQuantityStatus();
     public double updateQuantity(Filling fillDistillate) throws IllegalStateException;
