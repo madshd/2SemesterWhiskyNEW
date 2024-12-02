8a854bfd749909d357f0b7f0e9dc71a4acb96745
Mads Franzen <madsfranzen@live.dk>
2024-12-02 08:53:34 +0100

    Merge

--- Application/src/Controllers/Warehousing.java
+++ Application/src/Controllers/Warehousing.java
@@ -4,6 +4,7 @@ import Enumerations.IngredientType;
 import Enumerations.Unit;
 import Interfaces.Item;
 import Interfaces.StorageInterface;
+import Production.Distillate;
 import Storage.Storage;
 import Warehousing.Cask;
 import Warehousing.Supplier;
@@ -30,57 +31,57 @@ public abstract class Warehousing {
 		Warehousing.storage = storage;
 	}
 
-    /**
-     *
-     * @param name
-     * @param address
-     * @param description
-     * @param story
-     * @return
-     */
-    public static Supplier createSupplier(String name, String address, String description, String story){
-        return new Supplier(name,address,description,story);
-    }
+	/**
+	 *
+	 * @param name
+	 * @param address
+	 * @param description
+	 * @param story
+	 * @return
+	 */
+	public static Supplier createSupplier(String name, String address, String description, String story) {
+		return new Supplier(name, address, description, story);
+	}
 
-    /**
-     *
-     * @param caskID
-     * @param maxQuantity
-     * @param unit
-     * @param supplier
-     * @return
-     */
-    public static Cask createCask(int caskID, double maxQuantity, Unit unit, Supplier supplier){
-        Cask cask = new Cask(caskID,maxQuantity,unit,supplier);
-        storage.storeCask(cask);
-        return cask;
-    }
+	/**
+	 *
+	 * @param caskID
+	 * @param maxQuantity
+	 * @param unit
+	 * @param supplier
+	 * @return
+	 */
+	public static Cask createCask(int caskID, double maxQuantity, Unit unit, Supplier supplier) {
+		Cask cask = new Cask(caskID, maxQuantity, unit, supplier);
+		storage.storeCask(cask);
+		return cask;
+	}
 
-    /**
-     *
-     * @param distillate
-     * @return
-     */
-    public static List<Item> getCaskFitToDistillate(Distillate distillate){
-        //TODO
-        // Only cask fit for distillates should be returned.
+	/**
+	 *
+	 * @param distillate
+	 * @return
+	 */
+	public static List<Item> getCaskFitToDistillate(Distillate distillate) {
+		// TODO
+		// Only cask fit for distillates should be returned.
 
-        List<Item> casks = new ArrayList<>();
+		List<Item> casks = new ArrayList<>();
 
-        for (Item i : storage.getCasks()){
-            if (i.getRemainingQuantity() > 0){
-                casks.add(i);
-            }
-          
-        }
-      return casks;
-    }
+		for (Item i : storage.getCasks()) {
+			if (i.getRemainingQuantity() > 0) {
+				casks.add(i);
+			}
 
-    public static Warehouse createWarehouse(String name, String address){
-        Warehouse warehouse = new Warehouse(name,address);
-        storage.storeWarehouse(warehouse);
-        return warehouse;
-    }
+		}
+		return casks;
+	}
+
+	public static Warehouse createWarehouse(String name, String address) {
+		Warehouse warehouse = new Warehouse(name, address);
+		storage.storeWarehouse(warehouse);
+		return warehouse;
+	}
 
 	public static StorageRack createStorageRack(String rackID, int maxCapacity) {
 		StorageRack storageRack = new StorageRack(rackID, maxCapacity);
