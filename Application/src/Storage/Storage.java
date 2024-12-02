aa4d917342e1a4c4fad7528c488f957ef09886cc
Leander26 <jensleander26@icloud.com>
2024-12-02 09:09:37 +0100

    Removed an import that brakes building process.

--- Application/src/Storage/Storage.java
+++ Application/src/Storage/Storage.java
@@ -11,8 +11,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.checkerframework.checker.units.qual.A;
-
 import BatchArea.Formula;
 import BatchArea.Product;
 import BatchArea.TasteProfile;
